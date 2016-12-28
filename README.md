3dc6f97927cf1052204c7cac262dfa513c29875c
dgaitan <jdavid.gaitan@gmail.com>
2016-12-28 17:15:36 -0600

    last update to v1.0.2

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # nicaragua-map
 Simple maps of nicaragua with highchart library..
+version: 1.0.2
+
+Este pequeño mapa de Nicaragua, esta construido con la libreria de HighChart, que la pueden encontrar en [http://www.highcharts.com/](http://www.highcharts.com/ "Highchart")
+
+Para probar esto es necesario tener un pequeño server donde se puedan correr algunos pequeños json que contienen los path para formar los svg con highchart, para esto solamente hay que tener instalado npm para ejecutar `npm install` y de esta manera instalar unas pequeñas dependencias como lo es browser sync. una vez instalado esto, solamente hay que ejectuar `gulp` en la terminal y listo.
+
+
--- maps.js
+++ maps.js
@@ -1,127 +1,98 @@
 $(document).ready(function(){
-	$.get('maps/nicaragua-data.json', function(data){
-		var mapas = data;
-		var keys = [{
-				drilldown: 'chinandega',
-                'hc-key': 'ni-ci',
-                value: 10
-            }, {
-            	drilldown: 'leon',
-                'hc-key': 'ni-le',
-                value: 50
-            }, {
-            	drilldown: 'managua',
-                'hc-key': 'ni-mn',
-                value: 25
-            }, {
-            	drilldown: 'masaya',
-                'hc-key': 'ni-ms',
-                value: 70
-            }, {
-            	drilldown: 'carazo',
-                'hc-key': 'ni-ca',
-                value: 40
-            }, {
-            	drilldown: 'granada',
-                'hc-key': 'ni-gr',
-                value: 10
-            }, {
-            	drilldown: 'rio-san-juan',
-                'hc-key': 'ni-rio',
-                value: 60
-            }, {
-            	drilldown: 'rivas',
-                'hc-key': 'ni-rivas',
-                value: 70
-            }, {
-            	drilldown: 'chontales',
-                'hc-key': 'ni-co',
-                value: 80
-            }, {
-            	drilldown: 'boaco',
-                'hc-key': 'ni-bo',
-                value: 90
-            }, {
-            	drilldown: 'matagalpa',
-                'hc-key': 'ni-mt',
-                value: 10
-            }, {
-            	drilldown: 'esteli',
-                'hc-key': 'ni-es',
-                value: 11
-            }, {
-            	drilldown: 'nueva-segovia',
-                'hc-key': 'ni-ns',
-                value: 20
-            }, {
-            	drilldown: 'jinotega',
-                'hc-key': 'ni-ji',
-                value: 30
-            }, {
-            	drilldown: 'raan',
-                'hc-key': 'ni-raan',
-                value: 90
-            }, {
-            	drilldown: 'raas',
-                'hc-key': 'ni-raas',
-                value: 15
-            }, {
-            	drilldown: 'madriz',
-                'hc-key': 'ni-md',
-                value: 17
-            }];
 
-        $('#nicaragua').highcharts('Map', {
-        	series: [{
-        		data: keys,
-        		joinBy: 'hc-key',
-        		//mapData: Highcharts.maps['countries/ni/ni-all'],
-        		mapData: Highcharts.maps.nicaragua,
-                name: 'Nicaragua',
-        		dataLables: {
-        			enabled: true,
-        			format: '{this.data.name}'
-        		}
-        	}],
-        	drilldown: {
-        		series: mapas,
-        		activeDataLabelStyle: {
-        			color: '#FFFFFF',
-        			textDecoration: 'none',
-        			textOutline: '1px #000000'
-        		},
-        		drillUpButton: {
-        			relativeTo: 'spacingBox',
-        			position: {
-        				x: 0,
-        				y: 60
-        			}
-        		}
-        	},
-        	title: {
-        		text: 'Nicaragua'
-        	},
-        	legend: false,
-        	mapNavigation: {
-        		enabled: true,
-        		buttonOptions: {
-        			verticalAlign: 'bottom'
-        		}
-        	},
-        	plotOptions: {
-        		map: {
-        			states: {
-        				hover: {
-        					color: '#0DDEF2'
-        				}
-        			}
-        		}
-        	},
-            colorAxis: {
-                min: 0,
-                minColor: '#99DAFF',
-                maxColor: '#569CC4'
+    var test = Highcharts.maps.nicaragua;
+    $.each(test, function(i){
+        this.drilldown = this.key;
+        this.value = i;
+    });
+    console.log(test);
+
+    Highcharts.mapChart('nicaragua', {
+        chart: {
+            events: {
+                drilldown: function(e){
+                    if (!e.seriesOptions){
+                        var chart = this;
+                        mapkey = 'maps/' + e.point.drilldown;
+                        $.get(mapkey + '.json', function(data) {
+
+                            $.each(data, function (i) {
+                                this.value = i;
+                            });
+                            
+                            chart.addSeriesAsDrilldown(e.point, {
+                                name: e.point.name,
+                                data: data.data,
+                                dataLabels: {
+                                    enabled: true,
+                                    format: '{point.name}'
+                                }
+                            });
+                            console.log(data);
+                        });
+                    }
+                }
+            }
+        },
+        title: {
+            text: 'Mapa de Nicaragua'
+        },
+
+        subtitle: {
+            text: 'NIC',
+            floating: true,
+            align: 'right',
+            y: 50,
+            style: {
+                fontSize: '16px'
+            }
+        },
+
+        colorAxis: {
+            min: 0,
+            minColor: '#99DAFF',
+            maxColor: '#569CC4'
+        },
+
+        mapNavigation: {
+            enabled: true,
+            buttonOptions: {
+                verticalAlign: 'bottom'
+            }
+        },
+
+        plotOptions: {
+            map: {
+                states: {
+                    hover: {
+                        color: '#EEDD66'
+                    }
+                }
+            }
+        },
+        series: [{
+            data: test,
+            name: 'USA',
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }],
+        drilldown: {
+            activeDataLabelStyle: {
+                color: '#FFFFFF',
+                textDecoration: 'none',
+                textOutline: '1px #000000'
             },
-        });
-	});
+            drillUpButton: {
+                relativeTo: 'spacingBox',
+                position: {
+                    x: 0,
+                    y: 60
+                }
+            }
+        }
+    });
+
+	
 });
